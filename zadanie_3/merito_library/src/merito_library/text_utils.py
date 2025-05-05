e4a323435836fb07024dd061d37c0c44c123907a
matthewtop <matthewtop@users.noreply.github.com>
2025-05-05 23:11:33 +0200

    style: fix PEP8 issues && finished lab3

--- zadanie_3/merito_library/src/merito_library/text_utils.py
+++ zadanie_3/merito_library/src/merito_library/text_utils.py
@@ -1,8 +1,6 @@
-#src/merito_library/text_utils.py
-
-"""Simple module for text manipulation and analysis."""
 import re
-from typing import List, Dict, Union
+from typing import Dict, Union
+
 
 def count_words(text: str) -> int:
     """Count the number of words in a given text."""
@@ -10,6 +8,7 @@ def count_words(text: str) -> int:
         raise TypeError("Input must be a string.")
     words = re.findall(r'\b\w+\b', text)
     return len(words)
+
 
 def reverse_words(text: str) -> str:
     """Reverse the order of words in a given text."""
@@ -19,11 +18,13 @@ def reverse_words(text: str) -> str:
     reversed_words = ' '.join(reversed(words))
     return reversed_words
 
+
 def reverse_string(text: str) -> str:
     """Reverse the entire string."""
     if not isinstance(text, str):
         raise TypeError("Input must be a string.")
     return text[::-1]
+
 
 def count_characters(text: str) -> int:
     """Count the number of characters in a given text."""
@@ -31,12 +32,14 @@ def count_characters(text: str) -> int:
         raise TypeError("Input must be a string.")
     return len(text)
 
+
 def is_palindrome(text: str) -> bool:
     """Check if a given text is a palindrome."""
     if not isinstance(text, str):
         raise TypeError("Input must be a string.")
     cleaned_text = re.sub(r'\W+', '', text).lower()
     return cleaned_text == cleaned_text[::-1]
+
 
 def find_most_common_word(text: str) -> Dict[str, Union[str, int]]:
     """Find the most common word in a given text."""
